ab8107c59bcdf6fd22c4fbd7f4fb0cc880d4c743
Stanley Davies <stan07davies@gmail.com>
2025-09-29 22:40:22 +0100

    Fleshed out week summary.

--- sum/sum.c
+++ sum/sum.c
@@ -8,8 +8,29 @@
 #define CH_LF   10
 #define CH_NL   0
 
+// #define CL_BLACK       0       
+#define CL_RED         1       
+#define CL_GREEN       2       
+#define CL_ORANGE      3       
+//#define CL_BLUE        4       
+//#define CL_PURPLE      5       
+//#define CL_CYAN        6       
+//#define CL_L_GRAY      7       
+//#define CL_D_GRAY      8       
+#define CL_L_RED       9       
+#define CL_L_GREEN     10
+#define CL_YELLOW      11
+// #define CL_L_BLUE      12
+// #define CL_L_PURPLE    13
+// #define CL_L_CYAN      14
+#define CL_WHITE       15
+
 static float rweek(
         int             offset
+);
+
+static void colourise(
+        int             col
 );
 
 void sum(
@@ -17,7 +38,22 @@ void sum(
 ) {
         float t = rweek(0);
 
-        printf("So far this week you have spent £%.2f.\n", t);
+        printf("So far this week you have spent ");
+
+        if (t <= 40.f) {
+                colourise(CL_GREEN);
+        } else if (t <= 70.f) {
+                colourise(CL_L_GREEN);
+        } else if (t <= 100.f) {
+                colourise(CL_YELLOW);
+        } else {
+                colourise(CL_L_RED);
+        }
+
+        printf("£%.2f", t);
+
+        colourise(CL_WHITE);
+        printf(".\n");
 }
 
 static float rweek(
@@ -58,4 +94,15 @@ static float rweek(
         fname = NULL;
         
         return total;
+}
+
+static void colourise(
+        int             col
+) {
+        if (CL_WHITE == col) {
+                printf("\033[0m");
+                return;
+        }
+
+        printf("\033[%d;3%dm", col / 8, col % 8);
 }
