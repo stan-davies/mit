113ad4c8b9aafe40ee29714866a280ba7aceea54
Stanley Davies <stan07davies@gmail.com>
2025-11-28 23:52:31 +0000

    Added some extra information logging.

--- sum/sum.c
+++ sum/sum.c
@@ -5,6 +5,7 @@
 #include <stdio.h>
 
 #include "util/util.h"
+#include "util/bounds.h"
 
 #define CL_GREEN        2
 #define CL_L_GREEN      10
@@ -12,8 +13,13 @@
 #define CL_RED          9
 
 static void colourise(
-        int             period          ,
-        float           total
+        float           period          ,
+        float           total           ,
+        int             nline
+);
+
+static void ex_data(
+        float           spent
 );
 
 void sum(
@@ -29,23 +35,18 @@ void sum(
         for (int w = period; w > 0; --w) {
                 q = rweek(w - 1);
                 printf("Week %d: ", period - w + 1);
-                colourise(1, q);
+                colourise(1.f, q, TRUE);
                 t += q;
         }
 
         if (1 == period) {      // No need to give period summary for one week.
                                 // Give daily average spend instead.
-                time_t clock = time(NULL);
-                struct tm datetime = *localtime(&clock);
-                int day = datetime.tm_wday;             // Days since Sunday.
-                day = 0 == day ? 7 : day;               // Correct for Sunday.
-
-                printf("That is an average of £%.2f per day.\n", t / day);
+                ex_data(t);
                 return;
         }
 
         printf("In total over this period you have spent ");
-        colourise(period, t);
+        colourise((float)period, t, TRUE);
 }
 
 void sum_sv(
@@ -56,18 +57,47 @@ void sum_sv(
 }
 
 static void colourise(
-        int             period          ,
-        float           total
+        float           period          ,
+        float           total           ,
+        int             nline
 ) {
         int col = CL_RED;
 
-        if (total <= 40.f * period) {
+        if (total <= FIRST_B * period) {
                 col = CL_GREEN;
-        } else if (total <= 70.f * period) {
+        } else if (total <= (FIRST_B + B_WIDTH) * period) {
                 col = CL_L_GREEN;
-        } else if (total <= 100.f * period) {
+        } else if (total <= (FIRST_B + 2.f * B_WIDTH) * period) {
                 col = CL_ORANGE;
         }
         
-        printf("\033[%d;3%dm£%.2f\033[0m\n", col / 8, col % 8, total);
+        printf("\033[%d;3%dm£%.2f\033[0m", col / 8, col % 8, total);
+        
+        if (nline) {
+                printf("\n");
+        }
+}
+
+static void ex_data(
+        float           spent
+) {
+        time_t t = time(NULL);
+        struct tm datetime = *localtime(&t);
+        int day = datetime.tm_wday;             // Days since Sunday.
+        day = 0 == day ? 7 : day;               // Correct for Sunday.
+
+        printf("That is an average of ");
+        colourise((float)day / 7.f, spent / day, FALSE);
+        printf(" per day.\n");
+        
+        float cap = FIRST_B;
+        while (spent > cap) {
+                cap += B_WIDTH;
+        }
+
+        printf("To remain under the ");
+        colourise((float)day / 7.f, cap, FALSE);
+        printf(" limit this week, try to spend around £%.2f each day for the "
+               "rest of the week, or £%.2f in total.\n",
+                (cap - spent) / (7 - day), cap - spent);
 }
---
+++ util/bounds.h
@@ -0,0 +1,7 @@
+#ifndef BOUNDS_H
+#define BOUNDS_H
+
+#define FIRST_B 40.f
+#define B_WIDTH 30.f
+
+#endif
